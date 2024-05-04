465e842cfd7d4c70ebdb87c415d9c67e5931cca7
jerrito <jerrito0240@gmail.com>
2024-05-04 12:08:41 +0000

    add:cart & order routes

---
+++ src/routes/cart.ts
@@ -0,0 +1,35 @@
+import express, {Express, Request, Response, Router} from "express";
+
+
+const cartRoute:Router=express.Router();
+
+
+cartRoute.post("/cart",(req:Request,res: Response)=>{
+
+try{
+
+}catch(e){
+
+}
+})
+
+cartRoute.get("/cart",(req:Request,res: Response)=>{
+
+    try{
+    
+    }catch(e){
+        
+    }
+    })
+
+cartRoute.put("/cart",(req:Request,res: Response)=>{
+
+        try{
+        
+        }catch(e){
+            
+        }
+})
+
+
+module.exports=cartRoute;
---
+++ src/routes/order.ts
@@ -0,0 +1,36 @@
+import express, {Express, Request, Response, Router} from "express";
+
+
+const orderRoute:Router=express.Router();
+
+
+orderRoute.get("/orders",(req:Request,res: Response)=>{
+
+try{
+
+}catch(e){
+
+}
+})
+
+orderRoute.post("/orders",(req:Request,res: Response)=>{
+
+    try{
+    
+    }catch(e){
+        
+    }
+    })
+
+
+    orderRoute.get("/orders/:id",(req:Request,res: Response)=>{
+
+        try{
+        
+        }catch(e){
+            
+        }
+            })
+
+
+module.exports=orderRoute;
